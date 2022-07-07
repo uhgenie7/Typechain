6f3d7d3c2e849d3a88268b1bfb0c9ddb3a914f03
HyejinUm <72803184+uhj1993@users.noreply.github.com>
2022-07-07 13:01:47 +0900

    feat: d.ts에 따른 코드

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-// class Block {
-//   constructor(private data: string) {}
-//   static hello() {
-//     return "hi";
-//   }
-// }
+import { init, exit } from "myPackage";
 
-import { init } from "myPackage";
+init({
+  url: "true",
+});
+
+exit(1);
+
+localStorage.clear();
